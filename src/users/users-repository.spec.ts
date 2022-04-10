1ab2531f58286281004047cf73fbd00edc4b5fc1
kbmelo <kbmelo@outlook.com>
2022-04-10 19:04:34 -0300

    fix(create-user-test-repository) adding userCreate test

--- src/users/users-repository.spec.ts
+++ src/users/users-repository.spec.ts
@@ -27,6 +27,22 @@ describe('UsersRepository', () => {
     expect(provider).toBeDefined();
   });
 
+  it('should create an user with the correct info', async () => {
+    const userData = {
+      name: 'John Doe',
+      phone: '81912341234',
+      email: 'john.doe@gmail.com',
+      password: 'Test!234',
+    };
+
+    const newUser = await provider.createUser(userData);
+
+    expect(newUser.name).toBe(userData.name);
+    expect(newUser.phone).toBe(userData.phone);
+    expect(newUser.email).toBe(userData.email);
+    expect(newUser._id).toBeTruthy();
+  });
+
   afterAll(async () => {
     await closeInDatabaseConnection();
   });
